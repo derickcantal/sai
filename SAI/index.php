1f4aa13749c1198a262c9edec71b8d01541e739c
Derick Jan Ong Cantal <derickcantal@yahoo.com>
2014-03-16 06:57:25 +0800

    sai

---
+++ SAI/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/main.css"/>
+</head>
+<body>
+
+<div id="line"></div>
+
+<div class="login">
+<form>
+<p id="p_login">Username:<br>
+<input type="text" class="text_login" autofocus/><br>
+Password:<br>
+<input type="password" class="text_login"/><br>
+<input type="submit" value="Login" id="btn_login" />
+<input type="reset" value="Cancel" id="btn_cancel" /></p><br>
+</form>
+</div>
+
+<div id="line1"></div>
+
+</body>
+</html>
