3f65a6cde61071849680aaff6d71c627a454e2a1
Derick Jan Ong Cantal <derickcantal@yahoo.com>
2014-03-17 00:59:59 +0800

    insert.ready

--- connection.php
+++ connection.php
@@ -1,4 +1,12 @@
 <?php
 $connect = mysql_connect("localhost", "root", "") or die("Could not connect to server");
 $database = mysql_select_db("sai",$connect) or die("Could not locate database");
+
+$insertproduct = "INSERT INTO inventory SET Pname = '$pname',Nocase = '$nocase',Volume = '$volume',stocks = '$stocks',Price = '$price'";
+
+$insertuser = "INSERT INTO accounts SET Fname = '$fname', Lname = '$lname', Password = '$password', username = '$username'";
+
+$insertsales = "INSERT INTO sales SET PID = '$pid' , UID = '$uid', Qty = '$qty', Amount = '$amount', Total = '$total', _Date = '$date'";
+
 ?>
+
