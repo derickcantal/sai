3e425bc379a94ba0541fcf154925664c522ed427
Derick Jan Ong Cantal <derickcantal@users.noreply.github.com>
2014-03-16 15:13:09 +0800

    create index

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<link rel="icon" type="image/ico" href="img/sanmig.ico"></link>
+<link rel="stylesheet" type="text/css" href="css/main.css"/>
+<title>Sales And Inventory System - Vossler Enterprise</title>
+</head>
+<body>
+<header>
+	<img src="img/logo.png" id="head_logo"/>
+</header>
+<div id="line"></div>
+
+<div class="login">
+	<form>
+		<p id="p_login"><label>Username:<br></label>
+		<input type="text" class="text_login" autofocus/><br><br>
+		<label>Password:<br></label>
+		<input type="password" class="text_login"/><br></p>
+		<input type="submit" value="Login" id="btn_login" class="btn_loginform" />
+		<input type="reset" value="Cancel" id="btn_cancel" class="btn_loginform"/></p><br>
+	</form>
+</div>
+<div id="line1"></div>
+
+<footer>Sales and Inventory System</footer>
+</body>
+</html>
