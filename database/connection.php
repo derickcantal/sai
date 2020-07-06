db53819c147359ff2effdc277f2cb27842eecf32
Derick Jan Ong Cantal <derickcantal@users.noreply.github.com>
2020-07-06 21:26:28 +0800

    mysql to mysqli

--- database/connection.php
+++ database/connection.php
@@ -1,6 +1,18 @@
 <?php
-$connect = mysql_connect("localhost", "root", "") or die($myQuery."<br/><br/>".mysql_error());
-$database = mysql_select_db("sai",$connect) or die($myQuery."<br/><br/>".mysql_error());
+
+$connection = mysqli_connect("localhost", "root", "");
+
+if (!$connection) {
+    error_log("Failed to connect to MySQL: " . mysqli_error($connection));
+    die('Internal server error');
+}
+
+// 2. Select a database to use 
+$db_select = mysqli_select_db($connection, "sai");
+if (!$db_select) {
+    error_log("Database selection failed: " . mysqli_error($connection));
+    die('Internal server error');
+}
 
 ?>
 
